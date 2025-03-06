80ad4333d0329e799b95f364501ecc9b2953c6f4
itsmetohirr <itsmetohir@gmail.com>
2025-03-06 10:38:55 +0500

    Login done

--- ai_blog/views.py
+++ ai_blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
 from .forms import CustomUserCreationForm
 
 
@@ -8,6 +10,20 @@ def home_view(request):
 
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+
+
+
     return render(request, 'login.html')
 
 
